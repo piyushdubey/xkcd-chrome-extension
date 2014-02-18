64d7163cd7ab3d86389c2c00f3d4a69ec41b2aa2
Piyush Dubey <piyudubey@gmail.com>
2014-02-18 13:50:52 -0600

    removing redundant validation checks impertinent to chrome browser

--- js/popup.js
+++ js/popup.js
@@ -77,13 +77,6 @@ function createCORSRequest(method, url) {
     // "withCredentials" only exists on XMLHTTPRequest2 objects.
     xhr.open(method, url, true);
 
-  } else if (typeof XDomainRequest != "undefined") {
-
-    // Otherwise, check if XDomainRequest.
-    // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
-    xhr = new XDomainRequest();
-    xhr.open(method, url);
-
   } else {
 
     // Otherwise, CORS is not supported by the browser.
